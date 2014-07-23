c015f4ef13c0b9818ce74e0e0380788318592a9b
André Baudin-Laurencin <abaudin@gmail.com>
2014-07-23 15:30:05 +0200

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'signup'=> 'users#new'
+  resources :users
+
+  root "movies#index"
+  resources :movies do
+    resources :reviews
+  end
+end
---
+++ spec/features/list_users_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+
+describe "Viewing the list of users" do
+
+
+  it "shows the users" do
+    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com", password: 'abracadabra',password_confirmation:"abracadabra"))
+    user2 = User.create!(user_attributes(name: "Moe", email: "moe@example.com", password: "abracadabra",password_confirmation:"abracadabra"))
+    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com", password: "abracadabra",password_confirmation:"abracadabra"))
+
+
+    visit users_url
+
+
+    expect(page).to have_link(user1.name)
+    expect(page).to have_link(user2.name)
+    expect(page).to have_link(user3.name)
+  end
+
+
+end
